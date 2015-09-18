75397f8cae96b5bdee7ac4959d872a59bc2a7349
edly <rauledly@gmail.com>
2015-09-18 14:27:19 +0800

    unit

---
+++ unit/main.py
@@ -0,0 +1,30 @@
+﻿import random
+import socket
+import urllib2
+import cookielib
+import time
+import re
+import parse
+
+match = open("D:\\crawl for unit\\work\\match.txt", "r")
+all = open("D:\\crawl for unit\\work\\unit-abbr.txt", "r")
+
+output = open("D:\\crawl for unit\\work\\output2.txt", "w")
+
+a = set()
+cnt = 0
+while True:
+    num = match.readline()
+    if not num:
+        break
+    num = int(num)
+    line = match.readline()
+    line = line.lower()
+    a.add(line)
+while True:
+    line = all.readline()
+    if not line:
+        break
+    line = line.lower()
+    if not line in a:
+        output.write(line);
---
+++ unit/parse.py
@@ -0,0 +1,33 @@
+﻿import random
+import socket
+import urllib2
+import cookielib
+import time
+import re
+
+def adjust(s):
+    ret = ''
+    cnt = 0
+    for i in range(0, len(s)):
+        if (s[i] == '<'):
+            cnt = cnt + 1
+        else:
+            if (s[i] == '>'):
+                cnt = cnt - 1
+            else:
+                if (cnt == 0):
+                    ret = ret + s[i]
+    return ret
+
+def parse1(s, num, line):
+    str = r'is a unit|to other unit|from other unit'
+    str2 = r'Unit system|unit system'
+    infoList = re.findall(re.compile(str), s)
+    tmp = re.findall(re.compile(str2), s)
+    #for info in infoList:
+    #    file.write(adjust(info) + '\n')
+    if (len(infoList) > 0 and len(tmp) == 0):
+        print num, line
+    if (len(infoList) > 0):
+        return True
+    return False
